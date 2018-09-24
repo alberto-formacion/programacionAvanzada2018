653718e311face37f5fcd79834960dec04d0785d
Alumno <Alumno@CSL-P1-A14-14>
2018-09-24 20:56:35 +0200

    Subida Lunes 24/09

--- hibernateConfig/src/main/java/com/sanluis/hbm/hibernateConfig/App.java
+++ hibernateConfig/src/main/java/com/sanluis/hbm/hibernateConfig/App.java
@@ -10,14 +10,16 @@ public class App {
     	Configuration cfg = new Configuration()
     			.setProperty("hibernate.connection.driver_class", "com.mysql.jdbc.Driver")
     			.setProperty("hibernate.connection.url", "jdbc:mysql://localhost:3306/cdcol")
-    			.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQLDialect")
+    			.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQLDialect")//dialecto
     			.setProperty("hibernate.connection.username", "root")
     			.setProperty("hibernate.connection.password", "")
-    			.setProperty("hibernate.show_sql", "true")
+    			.setProperty("hibernate.show_sql", "true")//Opcional
     			.setProperty("hibernate.current_session_context_class", "thread")
-    			.addAnnotatedClass(Cd.class);
+    			.addAnnotatedClass(Cd.class);//Indicamos la clase
     			
     		SessionFactory sf = cfg.buildSessionFactory();
+    		
+    	
 
     }
 }
