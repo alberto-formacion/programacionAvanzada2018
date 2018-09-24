a0bb36c2eb603441f7e763157a3173b64b7058f7
Alberto Garcia de Cortazar Rouret <mum@192.168.0.42>
2018-09-24 15:38:46 +0200

    Hibernate Config

--- hibernateConfig/src/main/java/com/sanluis/hbm/hibernateConfig/App.java
+++ hibernateConfig/src/main/java/com/sanluis/hbm/hibernateConfig/App.java
@@ -1,13 +1,23 @@
 package com.sanluis.hbm.hibernateConfig;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.sanluis.hbm.hibernateConfig.model.Cd;
+
+public class App {
+    public static void main( String[] args ){
+    	Configuration cfg = new Configuration()
+    			.setProperty("hibernate.connection.driver_class", "com.mysql.jdbc.Driver")
+    			.setProperty("hibernate.connection.url", "jdbc:mysql://localhost:3306/cdcol")
+    			.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQLDialect")
+    			.setProperty("hibernate.connection.username", "root")
+    			.setProperty("hibernate.connection.password", "")
+    			.setProperty("hibernate.show_sql", "true")
+    			.setProperty("hibernate.current_session_context_class", "thread")
+    			.addAnnotatedClass(Cd.class);
+    			
+    		SessionFactory sf = cfg.buildSessionFactory();
+
     }
 }
