f9e493ba398d119194beecc358df787933530bc4
Alumno <Alumno@CSL-P1-A14-13>
2018-09-24 20:50:19 +0200

    Documentacion incluida

--- hibernateConfig/src/main/java/com/sanluis/hbm/hibernateConfig/App.java
+++ hibernateConfig/src/main/java/com/sanluis/hbm/hibernateConfig/App.java
@@ -8,15 +8,15 @@ import com.sanluis.hbm.hibernateConfig.model.Cd;
 public class App {
     public static void main( String[] args ){
     	Configuration cfg = new Configuration()
-    			.setProperty("hibernate.connection.driver_class", "com.mysql.jdbc.Driver")
-    			.setProperty("hibernate.connection.url", "jdbc:mysql://localhost:3306/cdcol")
-    			.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQLDialect")
-    			.setProperty("hibernate.connection.username", "root")
-    			.setProperty("hibernate.connection.password", "")
-    			.setProperty("hibernate.show_sql", "true")
-    			.setProperty("hibernate.current_session_context_class", "thread")
+    			.setProperty("hibernate.connection.driver_class", "com.mysql.jdbc.Driver")//Driver de conexion Sql
+    			.setProperty("hibernate.connection.url", "jdbc:mysql://localhost:3306/cdcol")//url de la base de datos
+    			.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQLDialect")//Dialecto de SQL
+    			.setProperty("hibernate.connection.username", "root")//Usuario de la bd
+    			.setProperty("hibernate.connection.password", "")//Pass del usuario
+    			.setProperty("hibernate.show_sql", "true")//Mostrar SQL
+    			.setProperty("hibernate.current_session_context_class", "thread")//El contexto de la sesion se guarda en el hilo principal de ejecucion
     			.addAnnotatedClass(Cd.class);
-    			
+    			//Todas menos show_sql son la conf. minima de hibernate
     		SessionFactory sf = cfg.buildSessionFactory();
 
     }
