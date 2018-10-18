cbe9468d2d380a628086620c8c5e1bfaa074c4e2
Alumno <Alumno@CSL-P1-A14-15>
2018-10-18 19:31:19 +0200

    Subida 18-10-18

---
+++ springmvc/src/main/java/com/sanluis/spring/springmvc/controller/TodoController.java
@@ -0,0 +1,29 @@
+package com.sanluis.spring.springmvc.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.sanluis.spring.springmvc.service.TodoService;
+import com.sanluis.spring.springmvc.vo.Todo;
+
+@Controller(value="/todos")
+public class TodoController {
+	
+	@Autowired
+	private TodoService todoService;
+	
+	@GetMapping
+	public String getTodos(Model model) {
+		
+		List<Todo> todos = todoService.getTodos();
+		
+		model.addAttribute("todos", todos);
+		
+		return "index";
+	}
+
+}
---
+++ springmvc/src/main/java/com/sanluis/spring/springmvc/vo/Todo.java
@@ -0,0 +1,42 @@
+package com.sanluis.spring.springmvc.vo;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+
+@Entity
+@NamedQueries({
+	@NamedQuery(name="todo.getTodos", query="SELECT t from Todo as t")
+})
+@Table(name="todos")
+public class Todo {
+	@Id
+	@GeneratedValue
+	@Column
+	private Integer id;
+	
+	@Column
+	private String text;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+	
+	
+}
